74d7ede0c0756f9f5a54aa1989207b63ac55bfd7
kgjerde <kristian.gjerde@gmail.com>
2019-03-10 22:25:55 +0100

    Updated corpus map height algorithm.
    Differentiaed between calendar and wall view.

--- inst/explorer/server/2_event_search_button/corpus_map_rendering.R
+++ inst/explorer/server/2_event_search_button/corpus_map_rendering.R
@@ -2,7 +2,7 @@ output$korpuskart <- renderPlot({
 
 source("./server/2_event_search_button/check_search_arguments.R", local = TRUE)
 
-    plot_fase_1 <-
+        plot_fase_1 <-
         visualiser_korpus(
             session_variables[[plot_mode$mode]],
             doc_df = session_variables$data_dok,
@@ -10,12 +10,21 @@ source("./server/2_event_search_button/check_search_arguments.R", local = TRUE)
             modus = plot_mode$mode
         )
 
-   session_variables$plot_build_info <-
+    session_variables$plot_build_info <-
         ggplot2::ggplot_build(plot_fase_1)$data[[2]]
 
-    value_for_slider <- (length(unique(session_variables$plot_build_info$ymax)) * 10) + 15
+    if (plot_mode$mode == "data_365") {
+        value_for_slider <-
+            ceiling((length(
+                unique(session_variables$plot_build_info$ymax)
+            ) * 10) + 15)
+    } else if (plot_mode$mode == "data_dok") {
+        value_for_slider <-
+            ceiling((length(
+                unique(session_variables$plot_build_info$ymax)
+            ) * 12) + 15)
+    }
 
-    value_for_slider <- ceiling(value_for_slider)
     # TODO Problem med omigjen-rendering ikke endelig løst
     # av commit 2b6ca83
     shiny::updateSliderInput(
@@ -27,21 +36,10 @@ source("./server/2_event_search_button/check_search_arguments.R", local = TRUE)
         max = NULL,
         step = NULL
     )
-    
-    
-    plot_fase_1 <-
-        visualiser_korpus(
-            session_variables[[plot_mode$mode]],
-            doc_df = session_variables$data_dok,
-            search_arguments = search_arguments,
-            modus = plot_mode$mode
-        )
-    
-    session_variables$plot_build_info <-
-        ggplot2::ggplot_build(plot_fase_1)$data[[2]]
-    
+
+
     plot_fase_1
-    
+
 }, #height = function() {  # https://github.com/rstudio/shiny/issues/650
 height =
     function(x) {
