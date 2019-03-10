e17ba31290cf8d7193750ec015c1eb7b54b610c6
kgjerde <kristian.gjerde@gmail.com>
2019-03-10 01:52:47 +0100

    Removed redundant file.
    This functionality has for some time been in js_auto_scrolling_to_top.R

--- inst/explorer/js_css/fixed_below_fixed_of_varying_height.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($){ //wait for the document to load
-        $('.class_doc_box .nav-tabs-custom').css({
-            'top' : $('.class_day_corpus .nav-tabs-custom').outerHeight() + 70 + 'px' //adjust the css rule for margin-top to equal the element height - 10px and add the measurement unit "px" for valid CSS
-        });
-});
-
-// From https://stackoverflow.com/a/44156191/8399819
--- inst/explorer/ui/css_js_import.R
+++ inst/explorer/ui/css_js_import.R
@@ -2,6 +2,5 @@ shiny::tags$head(
     shiny::includeCSS("js_css/app_css.css"),
     shiny::includeScript("js_css/box_width.js"),
     shiny::includeScript("js_css/enter_search.js"),
-    shiny::includeScript("js_css/jquery.scrollTo.js"),
-    shiny::includeScript("js_css/fixed_below_fixed_of_varying_height.js")
+    shiny::includeScript("js_css/jquery.scrollTo.js")
 )
