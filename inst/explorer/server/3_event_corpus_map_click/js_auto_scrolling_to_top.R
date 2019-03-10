324ddf12877efb28c8c56da3086639ac7d05f667
kgjerde <kristian.gjerde@gmail.com>
2019-03-10 16:11:38 +0100

    simplifying

--- inst/explorer/server/3_event_corpus_map_click/js_auto_scrolling_to_top.R
+++ inst/explorer/server/3_event_corpus_map_click/js_auto_scrolling_to_top.R
@@ -1,11 +1,15 @@
 # Autoscrolling til toppen når nytt dokument trykkes
 shinyjs::runjs(sprintf("$('.boxed_doc').scrollTo('#%s');", 1))
+shinyjs::runjs(sprintf("$('.KARR').scrollTo('#%s');", 1))
+
+
 
 if (plot_mode$mode == "data_365") {
 
     div_height <- session_variables$day_plot_height
+    max_height <- 200  # corresponds to max-height in css for #dag_kart
 
-    if (div_height > 200) div_height <- 200  # corresponds to max-height in css for #dag_kart
+    if (div_height > max_height) div_height <- max_height
 
     shinyjs::runjs(sprintf(
         "$('.class_doc_box .nav-tabs-custom').css({
