46f503c6440d9928d5b96c4665d65ea56fe94f1a
kgjerde <kristian.gjerde@gmail.com>
2019-05-07 00:32:03 +0200

    Made navigation link class less prone for accidental highlighting

--- inst/explorer/global/function_insert_doc_links.R
+++ inst/explorer/global/function_insert_doc_links.R
@@ -31,7 +31,7 @@ insert_doc_navigation_links <- function(tekst, antall_deler) {
     }
     ny <- paste0(
         sprintf(
-            "<span class =\'posisjons_korrigering\' id=\'%s\'></span>",
+            "<span class =\'___\' id=\'%s\'></span>",
             seq_len(antall_deler)
         ),
         ny_2
