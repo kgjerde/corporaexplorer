725c5a0c956a8ae0f5286ca3c87e85616331fd63
kgjerde <kristian.gjerde@gmail.com>
2019-05-09 16:43:55 +0200

    Fix plot at startup in line with changes in cb9ac148

--- inst/explorer/server/1_startup_actions.R
+++ inst/explorer/server/1_startup_actions.R
@@ -14,10 +14,19 @@ output$korpuskart <- shiny::renderPlot({
             search_arguments = search_arguments,
             modus = plot_mode$mode
         )
+
+    # Identifying the coordinates for the rectangles around date/document
+        # Depends on plot mode and # of search terms.
+    index_of_main_grid_rects <- dplyr::case_when(
+        plot_mode$mode == "data_365" ~ as.integer(length(ggplot2::ggplot_build(start_plot)$data) - 2),
+        plot_mode$mode == "data_dok" ~ as.integer(length(ggplot2::ggplot_build(start_plot)$data))
+    )
+
     session_variables$plot_build_info <-
-        ggplot2::ggplot_build(start_plot)$data[[2]]
+        ggplot2::ggplot_build(start_plot)$data[[index_of_main_grid_rects]]
+
     start_plot
-    
+
 }, height = function() {
     # https://github.com/rstudio/shiny/issues/650
     input$PLOTSIZE
