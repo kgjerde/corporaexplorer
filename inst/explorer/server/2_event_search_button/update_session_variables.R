da0afa40376d9323b962d447ab0f1a24dc2da6cf
kgjerde <kristian.gjerde@gmail.com>
2019-05-22 13:28:53 +0200

    "Reset" subset arguments when new search without subsetting
    - Avoids previous search arguments being checked in argument checking

--- inst/explorer/server/2_event_search_button/update_session_variables.R
+++ inst/explorer/server/2_event_search_button/update_session_variables.R
@@ -42,6 +42,13 @@ if (!is.null(input$subset_corpus)) {
     search_arguments$subset_terms <-
         to_lower_if_case_insensitive_search(search_arguments$raw_subset_terms) %>%
         clean_terms()
+} else {
+    # "Resetting" variables if no filtering:
+    search_arguments$subset_search = FALSE
+    search_arguments$subset_terms = NULL
+    search_arguments$subset_thresholds = NA
+    search_arguments$subset_custom_column = NA
+    search_arguments$raw_subset_terms = character(0)
 }
 
 # Search terms ------------------------------------------------------------
