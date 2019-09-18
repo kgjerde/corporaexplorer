36ee078f2935b5e9becf44e4f1eb2234401282df
kgjerde <kristian.gjerde@gmail.com>
2019-09-18 15:35:36 +0200

    improve print output

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -572,18 +572,21 @@ prepare_data.character <- function(dataset, ...) {
 #' @return Console-friendly output
 #' @keywords internal
 #' @export
-
 print.corporaexplorerobject <- function(x, ...) {
-cat("corporaexplorerobject\n")
-if (!is.null(x$corpus_name)) {
+  cat("corporaexplorerobject\n")
+  if (!is.null(x$corpus_name)) {
     cat("Corpus name:", x$corpus_name, "\n")
-}
+  }
 
-cat("Documents:", nrow(x$original_data$data_dok), "\n")
+  cat("Created by corporaexplorer version",
+      as.character(x[["version"]]),
+      "\n")
 
-if (!is.null(x[["original_matrix"]][["data_dok"]])) {
+  cat("Documents:", nrow(x$original_data$data_dok), "\n")
+
+  if (identical(x[["original_matrix"]][["data_dok"]], FALSE) == FALSE) {
     cat("Contains matrix with",
         length(x[["ordvektorer"]][["data_dok"]]),
         "unique tokens\n")
-}
+  }
 }
