733a5da8589dcaf9d826f49dab0d03d604d097e9
K <kristian.gjerde@gmail.com>
2019-11-20 17:56:42 +0100

    enter works with all search fields

--- inst/explorer/js_css/enter_search.js
+++ inst/explorer/js_css/enter_search.js
@@ -1,5 +1,5 @@
 $(document).keyup(function(event) {
-    if ($("#search_text_1").is(":focus") && (event.keyCode == 13)) {
+    if ($("#search_text_1, #search_text_2, #search_text_3, #search_text_4, #search_text_5, #search_text_6").is(":focus") && (event.keyCode == 13)) {
         $("#search_button").click();
     }
 });
