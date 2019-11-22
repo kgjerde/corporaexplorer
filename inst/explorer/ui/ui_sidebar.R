9ee5705007131275710da75585c24d1a77f778cb
K <kristian.gjerde@gmail.com>
2019-11-23 00:37:57 +0100

    minimum plot height was too low

--- inst/explorer/ui/ui_sidebar.R
+++ inst/explorer/ui/ui_sidebar.R
@@ -100,7 +100,7 @@ shinydashboard::dashboardSidebar(
         shiny::sliderInput(
             inputId = "PLOTSIZE",
             label = NULL,
-            min = 50,
+            min = 100,
             ticks = FALSE,
             step = 50,
             max = plot_size(loaded_data$original_data$data_dok,
