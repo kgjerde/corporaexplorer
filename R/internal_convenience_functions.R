5e2ff299eb23f5382e22882d16ff1acad38e7580
K <kristian.gjerde@gmail.com>
2019-11-28 12:21:01 +0100

    add internal convenience function

---
+++ R/internal_convenience_functions.R
@@ -0,0 +1,20 @@
+#' Retrieve the document data frame from a corporaexplorerobject
+#'
+#' @param x corporaexplorerobject
+#'
+#' @return data.frame
+#' @keywords internal
+get_df <- function(x, make_normal = TRUE) {
+    df <- x$original_data$data_dok
+
+    if (make_normal == TRUE) {
+        if (is.null(x$version)) {
+            df$Text <- df$Text_case
+            df$Text_case <- NULL
+        } else {
+            df$Text <- df$Text_original_case
+            df$Text_original_case <- NULL
+        }
+    }
+    return(df)
+}
