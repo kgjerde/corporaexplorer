63e043ccde076c083fef326680e1de5a25d9845d
K <kristian.gjerde@gmail.com>
2019-11-30 22:50:39 +0100

    url as per R CMD CHECK

--- R/demo_sotu.R
+++ R/demo_sotu.R
@@ -2,7 +2,7 @@
 #'
 #' Two demo apps exploring the United States Presidential State of the Union addresses.
 #'   The data are provided by the
-#'   \href{https://cran.r-project.org/web/packages/sotu/index.html}{sotu}
+#'   \href{https://CRAN.R-project.org/package=sotu}{sotu}
 #'   package, and include all adresses through 2016.
 #'   Interrupt R to stop the application (usually by pressing Ctrl+C or Esc).
 #'
