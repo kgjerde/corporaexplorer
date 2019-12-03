97e72d331e2cb64c605ab7303dc7df62585f0450
K <kristian.gjerde@gmail.com>
2019-12-03 16:25:26 +0100

    Improve print

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -599,29 +599,3 @@ prepare_data.character <-
       ...
     )
   }
-
-#' Print corporaexplorerobject
-#'
-#' @param x A corporaexplorerobject
-#'
-#' @return Console-friendly output
-#' @keywords internal
-#' @export
-print.corporaexplorerobject <- function(x, ...) {
-  cat("corporaexplorerobject\n")
-  if (!is.null(x$corpus_name)) {
-    cat("Corpus name:", x$corpus_name, "\n")
-  }
-
-  cat("Created by corporaexplorer version",
-      as.character(x[["version"]]),
-      "\n")
-
-  cat("Documents:", nrow(x$original_data$data_dok), "\n")
-
-  if (identical(x[["original_matrix"]][["data_dok"]], FALSE) == FALSE) {
-    cat("Contains matrix with",
-        length(x[["ordvektorer"]][["data_dok"]]),
-        "unique tokens\n")
-  }
-}
