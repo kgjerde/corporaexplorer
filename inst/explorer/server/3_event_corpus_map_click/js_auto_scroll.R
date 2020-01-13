3ebc96d939990f293542599f0a36c046a7248b84
K <kristian.gjerde@gmail.com>
2020-01-13 11:02:24 +0100

    "extra" tab autoscrolls to top

--- inst/explorer/server/3_event_corpus_map_click/js_auto_scroll.R
+++ inst/explorer/server/3_event_corpus_map_click/js_auto_scroll.R
@@ -3,3 +3,4 @@ shinyjs::runjs(sprintf("$('.boxed_doc_%s').scrollTo('#%s');",
                        plot_mode$mode,
                        1))
 shinyjs::runjs(sprintf("$('#dag_kart').scrollTo('%s');", 0))
+shinyjs::runjs(sprintf("$('#extra').scrollTo('%s');", 0))
