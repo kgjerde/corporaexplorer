2a4aa4d3ba0f5b521537c2b032f09096faf33719
kgjerde <kristian.gjerde@gmail.com>
2024-08-09 15:44:49 +0200

    cleaning

--- inst/explorer/tests/testthat/test-corpus_info_test.R
+++ inst/explorer/tests/testthat/test-corpus_info_test.R
@@ -1,9 +1,7 @@
 library(shinytest2)
 
 test_that("Corpus info works", {
-    app <- AppDriver$new(#app_dir = system.file("explorer",
-        #                      package = "corporaexplorer"),
-        name = "corpus_info_test")
+    app <- AppDriver$new(name = "corpus_info_test")
     app$set_inputs(search_text_1 = "document")
     app$set_inputs(case_sensitivity = TRUE)
     app$click("search_button")
@@ -17,8 +15,7 @@ test_that("Corpus info works", {
 })
 
 test_that("Corpus info custom column search works", {
-    app <- AppDriver$new(#"../..",
-        name = "corpus_info_test_custom")
+    app <- AppDriver$new(name = "corpus_info_test_custom")
     app$set_inputs(search_text_1 = "2--Title")
     app$click("search_button")
     app$set_inputs(corpus_box = "Corpus info")
@@ -29,7 +26,7 @@ test_that("Corpus info custom column search works", {
 })
 
 test_that("{shinytest2} recording: year filtering", {
-    app <- AppDriver$new("../..", name = "year filtering")
+    app <- AppDriver$new(name = "year filtering")
     app$set_inputs(date_slider = c(2012, 2020))
     app$click("search_button")
     app$set_inputs(corpus_box = "Corpus info")
