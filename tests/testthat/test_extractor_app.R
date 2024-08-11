f7c0aa5dc77bbfb9c8d655229325efd5e96052c4
kgjerde <kristian.gjerde@gmail.com>
2024-08-11 13:31:18 +0200

    to test CI check problem

--- tests/testthat/test_extractor_app.R
+++ tests/testthat/test_extractor_app.R
@@ -1,11 +1,11 @@
 # This file is for testing the applications in the inst/ directory.
 
-library(shinytest2)
-
-test_that("explorer app works", {
-    # Don't run these tests on the CRAN build servers
-    skip_on_cran()
-
-    appdir <- system.file(package = "corporaexplorer", "download")
-    test_app(appdir)
-})
+# library(shinytest2)
+#
+# test_that("explorer app works", {
+#     # Don't run these tests on the CRAN build servers
+#     skip_on_cran()
+#
+#     appdir <- system.file(package = "corporaexplorer", "download")
+#     test_app(appdir)
+# })
