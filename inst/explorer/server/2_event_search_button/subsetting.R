kgjerde/corporaexplorer

commit 54c4aca78a4ce8893a289e1579bc3b256e19f44b

Calendar plot fix
Fix bug where calendar plot of filtered corpus (with more than one filtering term) displays dates as if containing documents matching the filter terms -- when it actually only all documents that date *combined* satisfy the filter terms.

inst/explorer/server/2_event_search_button/subsetting.R
@@ -45,6 +45,17 @@ if (!is.null(input$subset_corpus)) {
                               search_arguments,
                               "data_dok",
                               session_variables)
+
+  # Telling the data_365 tibble which dates contain
+    # at least one document with all the subset terms:
+    non_empty_dates <- unique(session_variables$data_dok$Date)
+
+    session_variables$data_365$Day_without_docs[session_variables$data_365$Date %in% non_empty_dates == FALSE] <-
+      TRUE
+
+    session_variables$data_365$ID[session_variables$data_365$Date %in% non_empty_dates == FALSE] <-
+      0
+
   }
 }
     }

inst/explorer/global/function_filter_corpus.R
@@ -84,13 +84,10 @@ filtrere_korpus_pattern <- function(df,
       )
 
     if (modus == "data_dok") {
+      # Subsetting the tibble
        if (nrow(new_df) != 0) {
       new_df <- new_df[treff[, 1] > 0,]
        }
-    } else if (modus == "data_365") {
-      ikke_i_subset_indekser <- which(treff[, 1] == 0)
-      new_df$Day_without_docs[ikke_i_subset_indekser] <- TRUE
-      new_df$ID[ikke_i_subset_indekser] <- 0
     }
 
     if (nrow(new_df) == 0) {  # For å ikke sette igang subseeting av tom df

commit e3e5b6ccec04a2d9e5fc50f49e59a949575e51f4

Fix plot error when combining 2 search terms with filter text
Bug introduced in 54c4aca where cx_ID = 0 is set for dates without
matching documents. This causes improve_visualisation_of_2_terms to
group all empty dates together, breaking the assumption that each
cx_ID group has exactly 2 rows (one per search term).

Remove the cx_ID = 0 assignment. Day_without_docs = TRUE is sufficient
to mark dates for hiding in the visualization.

inst/explorer/server/2_event_search_button/subsetting.R
@@ -117,9 +117,6 @@ if (search_arguments$subset_search == TRUE) {
     session_variables$data_365$Day_without_docs[session_variables$data_365$Date %in% non_empty_dates == FALSE] <-
       TRUE
 
-    session_variables$data_365$cx_ID[session_variables$data_365$Date %in% non_empty_dates == FALSE] <-
-      0
-
   }
   }
     #}