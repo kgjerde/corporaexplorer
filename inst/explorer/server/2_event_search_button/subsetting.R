kgjerde/corporaexplorer

commit a1f59db4fa3f3412eeb99e92664b8ad2679b5ba3

Fix filter corpus functionality broken in a4df9dc
The bslib migration (a4df9dc) removed the subset_corpus toggle panel
that revealed the filter text input. Code paths still checked for it
via input$subset_corpus. Replace all checks with search_arguments$subset_search,
which is set based on whether filter text is present.

Changes:
- subsetting.R: Replace input$subset_corpus checks with subset_search
- functions_checking_input.R: Same replacement in check_all_input()
- check_search_arguments.R: Simplify nested if to use subset_search
- functions_ui_management.R: Remove dead observers for inputs that no
  longer exist (adjust_plotsize, subset_corpus, more_terms_button,
  antall_linjer)

inst/explorer/server/2_event_search_button/subsetting.R
@@ -2,7 +2,7 @@
 # Sidebar checkbox filtering, if any --------------------------------------
 
 # Checking for presence of checkboxes
-if (!is.null(input$subset_corpus)) {
+if (search_arguments$subset_search == TRUE) {
   if (!is.null(UI_FILTERING_CHECKBOXES)) {
 
     for (i in seq_along(UI_FILTERING_CHECKBOXES$column_names)) {
@@ -84,9 +84,7 @@ session_variables[[plot_mode$mode]] <-
 
 # Filtering by filter pattern if present ----------------------------------
 
-if (!is.null(input$subset_corpus)) {
-  search_arguments$subset_search <- TRUE
-
+if (search_arguments$subset_search == TRUE) {
   session_variables[[plot_mode$mode]] <-
     filtrere_korpus_pattern(session_variables[[plot_mode$mode]],
                             search_arguments,

inst/explorer/server/2_event_search_button/check_search_arguments.R
@@ -6,10 +6,8 @@ if (search_arguments$all_ok == FALSE) {
 
     validate_thresholds(isolate(search_arguments$raw_highlight_terms))
 
-    if (!is.null(isolate(input$subset_corpus))) {
-        if (isolate(input$subset_corpus == 'Yes')) {
-            validate_thresholds(isolate(search_arguments$raw_subset_terms))
-        }
+    if (search_arguments$subset_search == TRUE) {
+        validate_thresholds(isolate(search_arguments$raw_subset_terms))
     }
 
     validate_column_names()

inst/explorer/server/functions_checking_input.R
@@ -114,7 +114,7 @@ validate_search_term_length <- function() {
 #' @return Logical
 check_all_input <- function() {
     all_terms <- search_arguments$raw_highlight_terms
-    if (!is.null(input$subset_corpus)) {
+    if (search_arguments$subset_search == TRUE) {
         all_terms <- c(all_terms, search_arguments$raw_subset_terms)
     }
     if (length(all_terms) > 0) {

inst/explorer/server/functions_ui_management.R
@@ -53,21 +53,6 @@ doc_title_non_date_based_corpora <- function(min_rad) {
   return(title)
 }
 
-# Avoiding flashing sidebar elements on load (w/css) ----------------------
-shiny::observeEvent(input$adjust_plotsize, {
-    change_ui_class(".plotsize_field", property = "display", "inline")
-})
-shiny::observeEvent(input$subset_corpus, {
-    change_ui_class(".subset_field", property = "display", "inline")
-})
-shiny::observeEvent(input$more_terms_button, {
-    change_ui_class(".more_terms_field", property = "display", "inline")
-})
-shiny::observeEvent(input$antall_linjer, {
-    change_ui_class(".additional_search_terms", property = "display", "inline")
-})
-
-
 # Corpus info tab: edit plot legend keys UI -------------------------------
 observe({
   shinyjs::hide("edit_info_plot_legend_keys")

commit 267bcf63b55a2632efb9da57dcc3cfb70003b948

Fix checkbox filtering broken in a1f59db
The fix in a1f59db replaced input$subset_corpus checks with
subset_search, but subset_search was only set when filter text
was present. This broke checkbox filtering (UI_FILTERING_CHECKBOXES)
which should work independently.

Now subset_search is set to TRUE when any checkbox is unchecked,
restoring previous behavior where the flag reflects actual filter
state rather than a UI toggle.

Also fix redundant condition (subset_search == TRUE | subset_search == TRUE).

inst/explorer/server/2_event_search_button/subsetting.R
@@ -1,19 +1,18 @@
 
 # Sidebar checkbox filtering, if any --------------------------------------
 
-# Checking for presence of checkboxes
-if (search_arguments$subset_search == TRUE) {
-  if (!is.null(UI_FILTERING_CHECKBOXES)) {
+# Checkbox filtering works independently of filter text
+if (!is.null(UI_FILTERING_CHECKBOXES)) {
 
-    for (i in seq_along(UI_FILTERING_CHECKBOXES$column_names)) {
-      # Do nothing if every box checked
-      if (!all(UI_FILTERING_CHECKBOXES$values[[i]] %in% input[[paste0("type_", i)]])) {
-      # If some unchecked, filter:
-        session_variables$data_dok <-
-          session_variables$data_dok[
-            session_variables$data_dok[[UI_FILTERING_CHECKBOXES$column_names[i]]] %in% input[[paste0("type_", i)]],
-          ]
-      }
+  for (i in seq_along(UI_FILTERING_CHECKBOXES$column_names)) {
+    # Do nothing if every box checked
+    if (!all(UI_FILTERING_CHECKBOXES$values[[i]] %in% input[[paste0("type_", i)]])) {
+    # If some unchecked, filter:
+      search_arguments$subset_search <- TRUE
+      session_variables$data_dok <-
+        session_variables$data_dok[
+          session_variables$data_dok[[UI_FILTERING_CHECKBOXES$column_names[i]]] %in% input[[paste0("type_", i)]],
+        ]
     }
   }
 }
@@ -95,7 +94,7 @@ if (search_arguments$subset_search == TRUE) {
 # Ekstra filtrering av data_dok ved subsetting i data_365: ----------------
 
   if (plot_mode$mode == "data_365") {
-      if (search_arguments$subset_search == TRUE | search_arguments$subset_search == TRUE) {
+      if (search_arguments$subset_search == TRUE) {
 
     session_variables$data_dok <-
 